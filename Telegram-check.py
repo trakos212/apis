0834e1834a6d629c985dd5fdee69976198caed51
Vodka-X <95764863+Vodka77@users.noreply.github.com>
2022-01-20 20:21:37 +0200

    main.py

---
+++ Telegram-check.py
@@ -0,0 +1,16 @@
+from flask import Flask,request
+import requests,os
+app = Flask(__name__)
+@app.route('/Telegram', methods=['GET'])
+def home_page():
+    user = str(request.args.get('user'))
+    url = requests.get(f'https://t.me/{user}').text
+    if 'tgme_username_link' in url:
+        data = {'data':'Valid User','result':'true'}
+        return data
+    else:
+        data = {'data':'Taken User','resut':'true'}
+        return data
+
+if __name__ == '__main__':             
+    app.run()        
