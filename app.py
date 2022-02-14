456ef97eac17d366de30fc06734f9636da2ea533
Vodka-X <95764863+Vodka77@users.noreply.github.com>
2022-02-14 16:07:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,36 +12,29 @@ def home_page():
     else:
         data = {'data':'Taken User','resut':'true'}
         return data
-@app.route('/Instagram/data/',methods=['GET'])  
-def user_insta():
-    user = request.args.get('user')
-    url = f"https://www.instagram.com/{user}/?__a=1"
-    head = {
+@app.route('/Instagran/Date/')
+def app2():
+    p = request.args.get('user')
+    import requests, secrets
+    from user_agent import generate_user_agent
+    rs = requests.session()
+    headers = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'accept-encoding': 'gzip, deflate, br',
-    'accept-language': 'en-US,en;q=0.9',
-    'cache-control': 'max-age=0',
-    'cookie': 'mid=YbysKAALAAGnsY7iGX9WEyOe8AaT; ig_did=EF4C2AFE-037F-4E1B-A158-C08728818708; ig_nrcb=1; ds_user_id=50787014839; csrftoken=UToGyPsqeTDVOZ7RVJNpWfFjMVUkdHn3; sessionid=50787014839%3Ajhdr3dv7iFRPYb%3A21; rur="RVA\05450787014839\0541672416874:01f7f7d72f9624862a45b7c7ed6092af21b367cd6ec4806e50222845967307214f80dac8"',
-    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Microsoft Edge";v="96"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': "Windows",
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'none',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62'
+    'accept-language': 'en-US,en;q=0.9,ar;q=0.8',
+    'cookie': f'csrftoken={secrets.token_hex(8)*2}; sessionid={secrets.token_hex(8)*2};',
+    'user-agent': generate_user_agent(),
     }
-    request1 = requests.get(url,headers=head,data={'__a': '1'}).json()
-    id_pro = request1['logging_page_id'].split('_')[1]
-    get = request1['graphql']
-    iid = get['user']
-    idd = iid['edge_follow']
-    iid1 = iid['edge_followed_by']
-    followed = iid1['count']
-    follow = idd['count']
-    photo = iid['profile_pic_url']
-    data = {'ok':'true','result':{'Followers':followed,'Following':follow,'Id':id_pro,'Photo':photo}}    
+    url = f'https://www.instagram.com/{p}/?__a=1'
+    req = rs.get(url,headers=headers).json()
+    dady = str(req['logging_page_id']).split('_')[1]
+
+    lord = f'https://o7aa.pythonanywhere.com/?id={dady}'
+
+    dadyx = rs.get(lord)
+    dewtools = dadyx.json()
+    datee = dewtools['data']
+    data  {'date':datee,'status':'loaded'}
     return data
 if __name__ == '__main__':             
     app.run(debug=True)        
