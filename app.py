25a6b25ef22b61753134050515cef9061a5f7ea1
Vodka-X <95764863+Vodka77@users.noreply.github.com>
2022-02-15 11:33:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,30 +13,6 @@ def home_page():
     else:
         data = {'data':'Taken User','resut':'true'}
         return data
-@app.route('/Instagran/Date/')
-def app2():
-    p = request.args.get('user')
-    import requests, secrets
-    from user_agent import generate_user_agent
-    rs = requests.session()
-    headers = {
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'accept-encoding': 'gzip, deflate, br',
-    'accept-language': 'en-US,en;q=0.9,ar;q=0.8',
-    'cookie': f'csrftoken={secrets.token_hex(8)*2}; sessionid={secrets.token_hex(8)*2};',
-    'user-agent': generate_user_agent(),
-    }
-    url = f'https://www.instagram.com/{p}/?__a=1'
-    req = rs.get(url,headers=headers).json()
-    dady = str(req['logging_page_id']).split('_')[1]
-
-    lord = f'https://o7aa.pythonanywhere.com/?id={dady}'
-
-    dadyx = rs.get(lord)
-    dewtools = dadyx.json()
-    datee = dewtools['data']
-    data  {'date':datee,'status':'loaded'}
-    return data
 @app.route('/user-agent')
 def arabicge():
     r = str(''.join((generate_user_agent() for i in range(1))))
