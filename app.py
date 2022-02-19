3c1175d931c16d65e9272ab6b2e53de2ed065a0d
TRAKOS CSI <69827418+mhdeiiking@users.noreply.github.com>
2022-02-19 21:48:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -66,5 +66,51 @@ def log():
     }
     reqq = requests.post(url2, headers=headers, data=data2).json()
     return reqq
+
+import requests
+import flask 
+from flask import *
+
+app = Flask(__name__)
+
+@app.route("/f")
+def Login_Facebook():
+    email = request.args.get("email")
+    passs = request.args.get("pass")
+    s = requests.session()
+    req = s.post(
+        url = "https://b-api.facebook.com/method/auth.login",
+        headers={
+    'content-type':'application/x-www-form-urlencoded',
+    'authority': 'b-api.facebook.com', 
+    'accept-language': 'en-US,en;q=0.9', 
+    'authorization': 'OAuth 200424423651082|2a9918c6bcd75b94cefcbb5635c6ad16',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko) [FBAN/MessengerLite;FBAV/115.0.0.2.114;FBPN/com.facebook.mlite;FBLC/ar_EG;FBBV/257412622;FBCR/Orange - STAY SAFE;FBMF/Xiaomi;FBBD/xiaomi;FBDV/Redmi 7;FBSV/9;FBCA/arm64-v8a:null;FBDM/{density=2.0,width=720,height=1369};]',
+        },
+        data=f"email={email}&password={passs}&credentials_type=password&error_detail_type=button_with_disabled&format=json&device_id=cdc4558c-4dd4-4fd0-9ba6-d09e0223d5e5&generate_session_cookies=1&generate_analytics_claim=1&generate_machine_id=1&method=auth.login"
+    )
+    if "Invalid username or password" in req.text:
+        data = {
+        "status":"Invalid username or password",
+        }
+        return data
+    elif "session_key" in req.text:
+        data = {
+        "status":"Success Login",
+        }
+        return data
+    elif "User must verify" in req.text:
+        data = {
+        "status":"User must verify",
+        }
+        return data
+    elif "User must confirm" in req.text:
+        data = {
+        "status":"User must confirm"
+        }
+        return data
+    else:
+        return "Error"
+
 if __name__ =='__main__':
     app.run(debug=True)
