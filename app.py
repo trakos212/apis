6c9ada288461eaa176805449d6cbc0e842788291
TRAKOS CSI <69827418+mhdeiiking@users.noreply.github.com>
2022-03-08 16:06:30 -0800

    Update app.py

--- app.py
+++ app.py
@@ -146,26 +146,9 @@ def telegraminfograber_page():
 @app.route("/order/")
 def setorder():
     username = request.args.get("username")
-    urrl = f"https://www.instagram.com/{username}/?__a=1"
-    headerss = {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "ar,en-US;q=0.9,en;q=0.8",
-        "cache-control": "max-age=0",
-        "cookie": """ig_nrcb=1; mid=YgJSbwALAAHmIewldap9YYmjMNQK; ig_did=EE0B775C-5314-44C7-A75A-5947BE5C8354; csrftoken=jDkCCw6jjGaPu7T9ZQK9DFuw8f8yx5a5; ds_user_id=51270159671; sessionid=51270159671%3AvJg7Iq4dcjaZys%3A29; shbid="17337\05451270159671\0541675855363:01f7082d9b4d7150c6b5a276c369217fd8c8b9608ba783ce0f8296a07ffef035571c401a"; shbts="1644319363\05451270159671\0541675855363:01f7088e5d280d9e850e7790aeff1a7841fcd48bc57367fa9e9e989d869571aeef028aa0"; rur="LDC\05451270159671\0541675879635:01f7eb8abafdff41eb532e8356f89f62130e64612f61e5e844d8b43b7867e82a347ceeca""",
-        "sec-ch-ua": """Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97""",
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "Windows",
-        "sec-fetch-dest": "document",
-        "sec-fetch-mode": "navigate",
-        "sec-fetch-site": "none",
-        "sec-fetch-user": "?1",
-        "upgrade-insecure-requests": "1",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
-    }
-    req_id = requests.get(urrl, headers=headerss).json()
-    pro = str(req_id['graphql']["user"]["profile_pic_url"])
-    id = str(req_id['graphql']['user']['id'])
+    rq = requests.get(f"https://echoar.ml/Apimedia/infon.php?user={username}").json()["Info"]
+    pro = rq['image']
+    id = rq['account_id']
 
     header = {
         'X-Access': 'cafegram',
